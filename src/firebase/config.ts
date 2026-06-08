ca4a0f4d5205a86db9c5ca721a0b438a32162344
Muneef Faisal <muneeffaisalav@gmail.com>
2026-06-08 13:45:37 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCCdRQdA8BwL4fA3uQfIKjekPDiCDfj

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "REPLACE_WITH_YOUR_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  apiKey: "AIzaSyCCdRQdA8BwL4fA3uQfIKjekPDiCDfjEqA",
+  authDomain: "studio-2364224586-e182c.firebaseapp.com",
+  projectId: "studio-2364224586-e182c",
+  storageBucket: "studio-2364224586-e182c.firebasestorage.app",
+  messagingSenderId: "959887333386",
+  appId: "1:959887333386:web:aef9650d96a75cda3d27f3"
 };
